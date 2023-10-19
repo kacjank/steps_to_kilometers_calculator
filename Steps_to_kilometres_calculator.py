715b39cab75ba66446a09c6a40325a8049a11d80
Kacper Jankowski <68730689+kacjank@users.noreply.github.com>
2023-10-19 21:27:18 +0200

    Update Steps_to_kilometres_calculator.py

--- Steps_to_kilometres_calculator.py
+++ Steps_to_kilometres_calculator.py
@@ -32,67 +32,67 @@ def counter(step_length):
 
 
 ############################
-
-print('Witaj w przeliczniku ilości kroków na kilometry!\n')
-name = input('Jak masz na imię? ')
-
-while not name.isalpha():
-    name = input('\nTo nie jest imię! Podaj swoje imię: ')
-
-correct_name = name.capitalize()
-
-if correct_name[-1] == 'a' and correct_name != 'Barnaba':
-    sex = 'female'
-    ending = 'przeszłaś'
-else:
-    sex = 'male'
-    ending = 'przeszedłeś'
-
-print(f'\nCześć {correct_name}!\n')
-
-question = input('Czy znasz długość swojego kroku? (T/N) ')
-
-# Zabezpieczenie poprawności wprowadzanych danych
-while question != 'T' and question != 't' and question.lower() != 'tak' and question != 'N' and question != 'n' and \
-        question.lower() != 'nie':
-    print('\nBłędna odpowiedź. Udziel poprawnej odpowiedzi!')
+if __name__=='__main__':
+    print('Witaj w przeliczniku ilości kroków na kilometry!\n')
+    name = input('Jak masz na imię? ')
+    
+    while not name.isalpha():
+        name = input('\nTo nie jest imię! Podaj swoje imię: ')
+    
+    correct_name = name.capitalize()
+    
+    if correct_name[-1] == 'a' and correct_name != 'Barnaba':
+        sex = 'female'
+        ending = 'przeszłaś'
+    else:
+        sex = 'male'
+        ending = 'przeszedłeś'
+    
+    print(f'\nCześć {correct_name}!\n')
+    
     question = input('Czy znasz długość swojego kroku? (T/N) ')
-
-if question == 'T' or question == 't' or question.lower() == 'tak':
-    print('\nTo świetnie!')
-    step_length = input('W takim razie jaka jest długość Twojego kroku (w centymetrach)? ')
-    # Zabezpieczenie poprawności wprowadzanych danych
-    while not step_length.isdigit():
-        step_length = input('\nBłędne dane! Podaj długość kroku w centymetrach: ')
-    # step_length = int(step_length)
-    while int(step_length) > 100 or int(step_length) < 0:
-        step_length = input('Błędna wartość! Podaj poprawną wartość: ')
-    counter(int(step_length))
-else:
-    question = input('\nCzy w takim razie chcesz skorzystać z uśrednionych danych z internetu? (T/N) ')
+    
     # Zabezpieczenie poprawności wprowadzanych danych
     while question != 'T' and question != 't' and question.lower() != 'tak' and question != 'N' and question != 'n' and \
             question.lower() != 'nie':
-        print('\nUdziel odpowiedzi na pytanie!')
-        question = input('Czy chcesz skorzystać z uśrednionych danych z internetu? (T/N) ')
-    if question == 'n' or question == 'N' or question.lower == 'nie':
-        print('\nW takim razie wróć, gdy zmierzysz już długość swojego kroku albo zmienisz zdanie.')
-    else:
-        height = input('\nŚwietnie, w takim razie podaj swój wzrost (w centymetrach): ')
+        print('\nBłędna odpowiedź. Udziel poprawnej odpowiedzi!')
+        question = input('Czy znasz długość swojego kroku? (T/N) ')
+    
+    if question == 'T' or question == 't' or question.lower() == 'tak':
+        print('\nTo świetnie!')
+        step_length = input('W takim razie jaka jest długość Twojego kroku (w centymetrach)? ')
         # Zabezpieczenie poprawności wprowadzanych danych
-        while not height.isdigit():
-            height = input('\nBłędne dane! Podaj swój wzrost w centymetrach: ')
-        while int(height) > 250 or int(height) < 20:
-            height = input('Błędna wartość! Podaj poprawną wartość: ')
-        # Dopasowanie długości kroku do wzrostu
-        if int(height) <= 160:  # cm
-            step_length = 52  # cm
-        elif 160 < int(height) <= 170:
-            step_length = 55
-        elif 170 < int(height) <= 180:
-            step_length = 58
-        elif int(height) > 180:
-            step_length = 61
-        counter(step_length)
-
-input('\n(Wciśnij Enter aby zakończyć)')
+        while not step_length.isdigit():
+            step_length = input('\nBłędne dane! Podaj długość kroku w centymetrach: ')
+        # step_length = int(step_length)
+        while int(step_length) > 100 or int(step_length) < 0:
+            step_length = input('Błędna wartość! Podaj poprawną wartość: ')
+        counter(int(step_length))
+    else:
+        question = input('\nCzy w takim razie chcesz skorzystać z uśrednionych danych z internetu? (T/N) ')
+        # Zabezpieczenie poprawności wprowadzanych danych
+        while question != 'T' and question != 't' and question.lower() != 'tak' and question != 'N' and question != 'n' and \
+                question.lower() != 'nie':
+            print('\nUdziel odpowiedzi na pytanie!')
+            question = input('Czy chcesz skorzystać z uśrednionych danych z internetu? (T/N) ')
+        if question == 'n' or question == 'N' or question.lower == 'nie':
+            print('\nW takim razie wróć, gdy zmierzysz już długość swojego kroku albo zmienisz zdanie.')
+        else:
+            height = input('\nŚwietnie, w takim razie podaj swój wzrost (w centymetrach): ')
+            # Zabezpieczenie poprawności wprowadzanych danych
+            while not height.isdigit():
+                height = input('\nBłędne dane! Podaj swój wzrost w centymetrach: ')
+            while int(height) > 250 or int(height) < 20:
+                height = input('Błędna wartość! Podaj poprawną wartość: ')
+            # Dopasowanie długości kroku do wzrostu
+            if int(height) <= 160:  # cm
+                step_length = 52  # cm
+            elif 160 < int(height) <= 170:
+                step_length = 55
+            elif 170 < int(height) <= 180:
+                step_length = 58
+            elif int(height) > 180:
+                step_length = 61
+            counter(step_length)
+    
+    input('\n(Wciśnij Enter aby zakończyć)')
